7c12bad2060df3aa9afe271ac1aa967c2a0c9765
Expedito Filho <exp.filho.junior@gmail.com>
2024-06-12 12:09:19 -0300

    Fix Validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -7,6 +7,7 @@ import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -22,6 +23,7 @@ public class ProductDTO {
 	@NotBlank(message = "Campo requerido")
 	private String description;
 
+	@NotNull(message = "Campo requerido")
 	@Positive(message = "O preço deve ser positivo")
 	private Double price;
 
